8a66bb73914a15c797445d219ab9360b78cbb56f
maccordingley <46027969+maccordingley@users.noreply.github.com>
2018-12-31 14:04:56 +0000

    aded echos for build platform

--- ci/script.sh
+++ ci/script.sh
@@ -4,13 +4,20 @@
 
 set -e
 
-echo "Before Install Script"
+echo "Deciding How to build"
 
 if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+     echo "Building For OSX"
      npm run dist
 elif [[ ${TRAVIS_OS_NAME} == "linux" && ${TRAVIS_BUILD_PALTORM} == "windows" ]]; then
+    echo "Building for Windows"
     npm run dist --win --x64
+    echo ${TRAVIS_BUILD_PALTORM}
+    echo "end build platform"
 elif [[ ${TRAVIS_OS_NAME} == "linux" && ${TRAVIS_BUILD_PALTORM} == "linux" ]]; then
+    echo "Building for Linux"
+    echo ${TRAVIS_BUILD_PALTORM}
+    echo "end build platform"
     npm run dist
 else
     echo "Unknown OS"
