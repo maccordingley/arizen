20b5d2367d2f424cadcf19b0101cba9887151ee8
maccazuke <cto@bulknodes.com>
2019-01-10 13:10:08 +0000

    testing npx added wine to adons

--- ci/script.sh
+++ ci/script.sh
@@ -7,4 +7,14 @@ set -e
 #npm run dist
 
 #apt install -y wine
-npx electron-builder build -lw
+
+
+if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+    npx electron-builder build -m
+elif [[ ${TRAVIS_OS_NAME} == "linux" && ${PLATFORM} == "windows"]] then
+    npx electron-builder build -w
+elif [[ ${TRAVIS_OS_NAME} == "linux" && ${PLATFORM} == "linux"]] then
+    npx electron-builder build -l
+else
+    echo "Unknown OS"
+fi
