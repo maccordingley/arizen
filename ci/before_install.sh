a0715f6047a4c6cedc4ea7edc7a4eb101091e897
maccazuke <cto@bulknodes.com>
2019-01-10 12:45:16 +0000

    testing npx added wine to adons

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -16,6 +16,11 @@ elif [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     sudo apt-get update
     sudo apt-get install --no-install-recommends -y icnsutils graphicsmagick xz-utils 
     sudo apt install --install-recommends --allow-unauthenticated winehq-stable
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    sudo apt install apt-transport-https
+    echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+    sudo apt update
+    sudo apt install mono-devel
 else
     echo "Unknown OS"
 fi
